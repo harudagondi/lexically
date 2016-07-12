3bbfe51be782d772329d1e5e88357aa85dda556d
harudagondi <harudagondi@hotmail.com>
2016-07-12 14:40:35 +0800

    Added AJAX functionality

--- lexically/static/coffee/main.coffee
+++ lexically/static/coffee/main.coffee
@@ -2,4 +2,24 @@ $ ->
   $( "table" ).tablesorter()
   $( ":file" ).filestyle()
 
+  $( ":submit" ).click ->
+    parent = $( "#parent" ).val()
+    pos = $( "#pos" ).val()
+    meaning = $( "#meaning" ).val()
+    ipa = $( "#ipa" ).val()
+    notes = $( "#notes" ).val()
+    language = $( "#language" ).val()
+
+    $.ajax({
+      url: '/',
+      data: $('form').serialize(),
+      type: 'POST',
+      success: (response) ->
+        console.log(response)
+      error: (error) ->
+        console.log(error)
+      })
+
+    return
+
   return
--- lexically/static/js/main.js
+++ lexically/static/js/main.js
@@ -1,4 +1,24 @@
 $(function() {
   $("table").tablesorter();
   $(":file").filestyle();
+  $(":submit").click(function() {
+    var ipa, language, meaning, notes, parent, pos;
+    parent = $("#parent").val();
+    pos = $("#pos").val();
+    meaning = $("#meaning").val();
+    ipa = $("#ipa").val();
+    notes = $("#notes").val();
+    language = $("#language").val();
+    $.ajax({
+      url: '/',
+      data: $('form').serialize(),
+      type: 'POST',
+      success: function(response) {
+        return console.log(response);
+      },
+      error: function(error) {
+        return console.log(error);
+      }
+    });
+  });
 });
